12f017be70743ee58ff026c2d53bc30a5ca0f86f
Anh <tran.duc.anh@framgia.com>
2018-04-04 14:35:34 +0700

    Xu ly File trong Kotlin

---
+++ src/SanPham.kt
@@ -0,0 +1,24 @@
+package com.example.Springtutorial
+
+import java.io.BufferedWriter
+import java.io.OutputStreamWriter
+import java.io.FileOutputStream
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.io.FileInputStream
+
+class SanPham{
+    var ma:Int = 0
+    var ten:String = ""
+    var donGia:Double = 0.0
+    constructor()
+    constructor(ma: Int, ten: String, donGia: Double){
+        this.ma = ma;
+        this.ten = ten;
+        this.donGia = donGia;
+    }
+
+    override fun toString(): String {
+        return "$ma\t$ten\t$donGia"
+    }
+}
